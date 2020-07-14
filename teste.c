8997b66e12e92421d68c7715834ecd271df737c8
Matheus D Queiroz <32309799+mdqueiroz1@users.noreply.github.com>
2020-07-14 18:09:00 -0300

    adicionando arquivo de teste

---
+++ teste.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+int main(){
+    float num1, num2, aux, cont=0;
+    char operador;
+    printf("Informe a operacao:");
+    do{
+        if(cont==0){
+            fflush(stdin);
+            scanf("%f %c %f", &num1, &operador, &num2);
+        }else{
+            fflush(stdin);
+            scanf("%c %f", &operador, &num2);
+        }
+
+        
+        
+        switch (operador){
+        
+            case '+':{
+               
+                if(cont == 0)
+                    aux = num1 + num2;
+                else
+                    aux += num2;
+               
+                break;
+            }
+            
+            case '-':{
+                if(cont == 0)
+                    aux = num1 - num2;
+                else
+                    aux -= num2;
+                break;
+            }
+            
+            case '/':{
+                
+                if(num2 == 0){
+                    printf("Erro, nao e possivel fazer divisao com 0.\n");
+                    break;
+                }
+                
+                if(cont == 0)
+                    aux = num1 / num2;
+                else
+                    aux = aux / num2;
+                
+                break;
+            }
+
+            case '*':{
+                
+                if(cont == 0)
+                    aux = num1 * num2;
+                else
+                    aux *= num2;
+
+                break;
+            }
+        }
+        cont = 1;
+        printf("%.2f", aux);
+    }while(operador != 'c');
+    return 0;
+}
